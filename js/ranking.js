cd6f54d5d196811edc618dc6e410a055dcdb50e3
NguyenTanPhucK11 <16520954@gm.uit.edu.vn>
2019-05-05 13:19:52 +0700

    pagination

--- js/ranking.js
+++ js/ranking.js
@@ -1,17 +1,23 @@
 var config = {
-  apiKey: "AIzaSyBXhP9jGoX0UGV3KpBezbmywwayVehOn64",
-  authDomain: "tennis-d904d.firebaseapp.com",
-  databaseURL: "https://tennis-d904d.firebaseio.com",
-  projectId: "tennis-d904d",
-  storageBucket: "tennis-d904d.appspot.com",
-  messagingSenderId: "1070894637338"
+        apiKey: "AIzaSyAogqLlIWQqrewpigRKENk5IXELCW8W4ws",
+        authDomain: "tennis-9c684.firebaseapp.com",
+        databaseURL: "https://tennis-9c684.firebaseio.com",
+        projectId: "tennis-9c684",
+        storageBucket: "tennis-9c684.appspot.com",
+        messagingSenderId: "669160857608"
 };
-
+    //   var config = {
+    //     apiKey: "AIzaSyBXhP9jGoX0UGV3KpBezbmywwayVehOn64",
+    //     authDomain: "tennis-d904d.firebaseapp.com",
+    //     databaseURL: "https://tennis-d904d.firebaseio.com",
+    //     projectId: "tennis-d904d",
+    //     storageBucket: "tennis-d904d.appspot.com",
+    //     messagingSenderId: "1070894637338"
+    //   };
+    
 firebase.initializeApp(config);
-
-
+    
 var db = firebase.firestore();
-
 
 function storeDataRanking() {
 
@@ -41,74 +47,68 @@ function storeDataRanking() {
 
 function rRankingATP(){
 
-    var docRef = db.collection("Ranking-ATP").orderBy("currentRank", "asc")
-    docRef.get().then(function (querySnapshot) {
-    querySnapshot.forEach(function(data){
-        
-        document.querySelector("#name").innerHTML += `  
-        <div align=center>
-<table>
-        <tr>
-        <td width=30%>
-        ${data.data().playerName}
-        </td>        
-        </tr>
-        </table>
-        </div>
-        </br>
-        ` 
-        document.querySelector("#country").innerHTML += `
-        <div align=center>
-<table>
-        <tr width=30%>
-        <td>
-        ${data.data().playerCountry}
-        </td>        
-        </tr>
-        </table>
-        </div>
-        </br>
-        ` 
-        document.querySelector("#pre-rank").innerHTML += `
-        <div align=center>
-<table>
-        <tr>
-        <td width=15%>
-        ${data.data().prevRank}
-        </td>        
-        </tr>
-        </table>
-        </div>
-        </br>
-        ` 
-        document.querySelector("#rank").innerHTML += `
-        <div align=center>
-<table>
-        <tr>
-        <td width=15%>
-        ${data.data().currentRank}
-        </td>        
-        </tr>
-        </table>
-        </div>
-        </br>
-        ` 
-        document.querySelector("#point").innerHTML += `
-        <div align=center>
-<table>
-        <tr>
-        <td width=20%>
-        ${data.data().playerPoints}
-        </td>        
-        </tr>
-        </table>
-        </div>
-        </br>
-        ` 
-
-    });
-    });
-
+        $(function () {
+    
+                (async function (name) {
+            
+                    
+                    var container = $('#pagination-' + name);        
+                    var sources = async function () {
+                        
+                        result = []
+            
+                        var docRef = db.collection("Ranking-ATP").orderBy("currentRank", "asc")
+            
+                        await docRef.get().then(function (querySnapshot) {
+            
+                            querySnapshot.forEach(function (data) {
+                                var temp = `
+                                ${data.data().playerCountry}
+                                ${data.data().playerName}
+                                ${data.data().currentRank}
+                                ${data.data().prevRank}
+                                ${data.data().playerPoints}
+                                `;
+            
+                                result.push(temp);
+                            });
+                        });
+                      
+                        
+                        return result;
+                    }();
+            
+                    var options = {
+                        dataSource: await sources,
+                        callback: function (response, pagination) {
+                            window.console && console.log(response, pagination);
+            
+                            var dataHtml = '<ul>';
+            
+                            $.each(response, function (index, item) {
+                                dataHtml += '<li>' + item + '</li>';
+                            });
+            
+                            dataHtml += '</ul>';
+            
+                            container.prev().html(dataHtml);
+                        }
+                    };
+            
+                    //$.pagination(container, options);
+            
+                    container.addHook('beforeInit', function () {
+                        window.console && console.log('beforeInit...');
+                    });
+                    container.pagination(options);
+            
+                    container.addHook('beforePageOnClick', function () {
+                        window.console && console.log('beforePageOnClick...');
+                        //return false
+                    });
+                })('demo1');
+            
+            })
 
 }
 
